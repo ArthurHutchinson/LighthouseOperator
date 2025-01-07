e510fe9b4db029d83f0250e78b629a2995028dcb
ArthurHutchinson <arthur.y.hutchinson@gmail.com>
2025-01-06 22:09:32 -0500

    Reverted Back, I'll deal the conversion another time.

--- 42/media/lua/client/LighthouseTraitMain.lua
+++
@@ -1,9 +0,0 @@
--- This is the logic of when the player has the AntiLonely (Expected Loneliness) trait.
-function antilonelytrait()
-    local player = getPlayer();
-    if player:HasTrait("AntiLonely") and player:getBodyDamage():getUnhappynessLevel() > 80 then
-        player:getBodyDamage():setUnhappynessLevel(61);
-    end
-end
-
-Events.OnPlayerUpdate.Add(antilonelytrait);
